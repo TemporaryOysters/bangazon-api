af18268ae37edde089d63ab4dfe481dec702ec98
Trent Hand <livefree1983@gmail.com>
2017-01-25 14:18:23 -0600

    added two serializers

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from quickstart.models import ProductType, Product, PaymentType, BangOrder, OrderHasProducts
 
-class UserSerializer(serializers.ModelSerializer):
-
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'email', 'date_joined')
+
+class BangOrderSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = BangOrder
+        fields = ('status', 'user', 'payment_type')
+
+class ProductTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ProductType
+        fields = ('name',)
+
